0ccba5a11b377badafb70024232c0cf97bd30e86
Alan Medel <63024711+medelalan@users.noreply.github.com>
2020-06-11 11:01:35 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+  if(){//
+  
+  }
+  else{
+    echo 
+    '
+      <form action="">
+        <p>First Name <input type="text" name="FirstName" /></p>
+        <p><input type="submit" /></p>
+      </form>
+    ';
+  }
+?>
