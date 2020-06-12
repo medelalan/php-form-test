00e18bead4da226f3d146f1640fe105ea96055a6
Alan Medel <medelalan23@gmail.com>
2020-06-12 18:08:36 +0000

    Adds H1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   else{//show form
     echo 
     '
+      <h1>My Form</h1>
       <form action="">
         <p>First Name <input type="text" name="FirstName" /></p>
         <p><input type="submit" /></p>
